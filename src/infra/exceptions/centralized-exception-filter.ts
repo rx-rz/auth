b1c9a18a374ca587f5048cd2fb2e5016cf19ceed
rxrzzz <temiloluwagithub@gmail.com>
2024-07-18 14:26:21 +0100

    feat: :sparkles: added centralized error filter

---
+++ src/infra/exceptions/centralized-exception-filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class CentralizedExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : exception.message;
+
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './utils/schema-validation/validation.pipe';
+import { CentralizedExceptionFilter } from './infra/exceptions/centralized-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new CentralizedExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
