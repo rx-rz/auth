380eb2e280380352db7525637b9daba56527d93d
rxrzzz <temiloluwagithub@gmail.com>
2024-08-06 14:36:30 +0100

    test: :white_check_mark: added login controller tests

--- src/login/login.controller.spec.ts
+++ src/login/login.controller.spec.ts
@@ -1,20 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
+import { IdDto } from './schema';
+import { faker } from '@faker-js/faker';
 
 describe('LoginController', () => {
-  let controller: LoginController;
+  let loginController: LoginController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LoginController],
-      providers: [LoginService],
+      providers: [
+        {
+          provide: LoginService,
+          useValue: {
+            deleteLoginInstance: jest.fn().mockImplementation(() => {
+              return Promise.resolve({ success: true, login: {} });
+            }),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<LoginController>(LoginController);
+    loginController = module.get<LoginController>(LoginController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(loginController).toBeDefined();
+  });
+
+  describe('Delete login instance', () => {
+    const dto: IdDto = {
+      id: faker.string.uuid(),
+    };
+    it('should successfully delete a login instance', async () => {
+      await expect(loginController.deleteLoginInstance(dto)).resolves.toEqual(
+        expect.objectContaining({
+          success: true,
+          login: expect.any(Object),
+        }),
+      );
+    });
   });
 });
