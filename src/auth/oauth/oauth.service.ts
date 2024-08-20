0f5b441b87ce3a56063b7dbe3b4278babf4e1f06
rxrzzz <temiloluwagithub@gmail.com>
2024-08-20 17:21:01 +0100

    ci: :adhesive_bandage: husky temporarily removed

--- src/auth/oauth/oauth.service.ts
+++ src/auth/oauth/oauth.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { OAuthProviders } from '@prisma/client';
+import { OAuthRepository } from './oauth.repository';
 
 @Injectable()
 export class OauthService {
-  constructor(private readonly oauthProviderRepository: ){}
-  // private strategy: OAuthStrategy;
-  // constructor(
-  //   private readonly oauthFactory: OAuthStrategyFactory,
-  //   private projectId: string,
-  //   private providerName: OAuthProviders,
-  // ) {}
-  // async initialize(): Promise<void> {
-  //   this.strategy = await this.oauthFactory.createStrategy(this.projectId, this.providerName, '');
-  // }
+  constructor(private readonly oauthProviderRepository: OAuthRepository) {}
 }
